470e11422063a56e6219248c3b4f4cddfa0ca69f
omatheusfaria <lampersmatheusdk@gmail.com>
2025-05-24 10:10:34 -0300

    adicição do .env

--- aula4/app.py
+++ aula4/app.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
+import os
 from flask import Flask, render_template, request, session, redirect
 import mysql.connector
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -11,11 +15,13 @@ def raiz():
 def login():
     cpf = request.form['cpf']
     senha = request.form['password']
-    db = mysql.connector.connect(host='201.232.3.86',
-                                 port=5000,
-                                 user='usr_aluno',
-                                 password='usr_aluno123',
-                                 database='aula_fatec')
+    db = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        port=int(os.getenv('DB_PORT')),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
+    )
     mycursor = db.cursor()
     query = "select nome, cpf, senha from matheusfaria_tbusuarios where cpf = '" + cpf + "' and senha = '" + senha + "'"
     mycursor.execute(query)
@@ -33,11 +39,13 @@ def cadastrar_usuario():
     nome = request.form['txt_nome']
     cpf = request.form['txt_cpf']
     senha = request.form['txt_senha']
-    db = mysql.connector.connect(host='201.232.3.86',
-                                 port=5000,
-                                 user='usr_aluno',
-                                 password='usr_aluno123',
-                                 database='aula_fatec')
+    db = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        port=int(os.getenv('DB_PORT')),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
+    )
     mycursor = db.cursor()
     query = "insert into matheusfaria_tbusuario (nome, cpf, senha) values(%s, %s, %s)"
     values = (nome, cpf, senha)
@@ -49,11 +57,13 @@ def cadastrar_usuario():
 def lista_user():
     if session.get('logado') is None:
         return redirect('/')
-    db = mysql.connector.connect(host='201.232.3.86',
-                                 port=5000,
-                                 user='usr_aluno',
-                                 password='usr_aluno123',
-                                 database='aula_fatec')
+    db = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        port=int(os.getenv('DB_PORT')),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
+    )
     mycursor = db.cursor()
     query = 'select nome, cpf, senha, id from matheusfaria_tbusuario'
     mycursor.execute(query)
@@ -62,11 +72,13 @@ def lista_user():
 
 @app.route('/alterar_usuario/<user>')
 def alterar_usuario(user):
-    db = mysql.connector.connect(host='201.232.3.86',
-                                 port=5000,
-                                 user='usr_aluno',
-                                 password='usr_aluno123',
-                                 database='aula_fatec')
+    db = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        port=int(os.getenv('DB_PORT')),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
+    )
     mycursor = db.cursor()
     query = "select nome, cpf, id from matheusfaria_tbusuario where id =" + user
     mycursor.execute(query)
@@ -79,11 +91,13 @@ def update_usuario():
     nome = request.form['txt_nome']
     cpf = request.form['txt_cpf']
     senha = request.form['txt_senha']
-    db = mysql.connector.connect(host='201.232.3.86',
-                                 port=5000,
-                                 user='usr_aluno',
-                                 password='usr_aluno123',
-                                 database='aula_fatec')
+    db = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        port=int(os.getenv('DB_PORT')),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
+    )
     mycursor = db.cursor()
     query = "update matheusfaria_tbusuario set nome ='" + nome + "', cpf = '" + cpf + "', senha = '" + senha + "' where id = " + id
     mycursor.execute(query)
